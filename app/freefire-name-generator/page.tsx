0f3e257fa187d4d1aa98962eb2699fa380449315
henrique <henriquesantossander@gmail.com>
2026-05-15 08:34:39 -0300

    Adicionando limite na ferramenta free fire

--- app/freefire-name-generator/page.tsx
+++ app/freefire-name-generator/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useState } from "react";
+import { checkUsageLimit, increaseUsage } from "@/lib/checkUsageLimit";
 
 export default function FreeFireNameGenerator() {
   const [name, setName] = useState("");
   const [loading, setLoading] = useState(false);
 
   const generateName = async () => {
+    const result = await checkUsageLimit();
+
+    if (!result.allowed) return;
+
     setLoading(true);
     setName("");
 
@@ -22,12 +27,15 @@ export default function FreeFireNameGenerator() {
       setName("Erro ao gerar nome.");
     }
 
+    if (!result.isPro && result.email) {
+      await increaseUsage(result.email, result.currentCount);
+    }
+
     setLoading(false);
   };
 
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center px-6 py-20">
-
       <h1 className="text-4xl md:text-5xl font-bold mb-4 text-center">
         Gerador de Nome Free Fire
       </h1>
@@ -53,7 +61,6 @@ export default function FreeFireNameGenerator() {
 
       {name && (
         <div className="bg-zinc-900 border border-zinc-800 mt-12 p-8 rounded-3xl max-w-xl w-full text-center">
-
           <div className="grid gap-3">
             {name.split("\n").map((nick, index) => (
               <button
@@ -68,10 +75,8 @@ export default function FreeFireNameGenerator() {
               </button>
             ))}
           </div>
-
         </div>
       )}
-
     </main>
   );
 }
