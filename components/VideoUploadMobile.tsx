34e3c06c90b484d2d19c80fae23381a69210de66
henrique <henriquesantossander@gmail.com>
2026-07-02 00:21:47 -0300

    Melhora layout mobile do Video IA

--- components/VideoUploadMobile.tsx
+++ components/VideoUploadMobile.tsx
@@ -2,6 +2,7 @@ type Props = {
   file: File | null;
   setFile: (file: File | null) => void;
 };
+import { ImagePlus } from "lucide-react";
 
 export default function VideoUploadMobile({
   file,
@@ -9,11 +10,13 @@ export default function VideoUploadMobile({
 }: Props) {
   return (
     <>
-      <h2 className="mb-5 text-2xl font-bold text-zinc-900">
-        1. Envie uma imagem
-      </h2>
+      <h2 className="mb-4 text-xl font-bold text-zinc-900">
+  1. Envie uma imagem
+</h2>
 
-      <label className="mb-6 flex h-56 cursor-pointer flex-col items-center justify-center rounded-3xl border-2 border-dashed border-zinc-300 bg-zinc-50 transition hover:bg-zinc-100">
+      <label 
+  className="flex h-56 w-full cursor-pointer flex-col items-center justify-center rounded-3xl border-2 border-dashed border-zinc-300 bg-zinc-50 transition hover:bg-zinc-100"
+>
 
         {file ? (
           <img
@@ -23,15 +26,20 @@ export default function VideoUploadMobile({
           />
         ) : (
           <>
-            <span className="text-5xl">📷</span>
+            <div className="mb-5 flex h-20 w-20 items-center justify-center rounded-3xl bg-violet-100">
+  <ImagePlus
+    size={42}
+    className="text-violet-600"
+  />
+</div>
 
-            <span className="mt-5 text-xl font-semibold text-zinc-700">
-              Toque para enviar uma imagem
-            </span>
+            <span className="mt-2 text-center text-2xl font-bold text-zinc-800">
+Toque para enviar uma imagem
+</span>
 
-            <span className="mt-2 text-zinc-500">
-              JPG • PNG • WEBP
-            </span>
+            <span className="mt-4 text-base font-medium tracking-wide text-zinc-500">
+JPG • PNG • WEBP
+</span>
           </>
         )}
 
@@ -42,6 +50,15 @@ export default function VideoUploadMobile({
           className="hidden"
         />
       </label>
+      {file && (
+  <div className="mt-5 overflow-hidden rounded-3xl border border-zinc-200 bg-white shadow-sm">
+    <img
+      src={URL.createObjectURL(file)}
+      alt="Preview"
+      className="h-64 w-full object-cover"
+    />
+  </div>
+)}
     </>
   );
 }
